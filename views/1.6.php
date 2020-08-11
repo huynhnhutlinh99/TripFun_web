2a6e092103f79eb6906f39d740648356158b8d29
noname <l.h.thuong181@gmail.com>
2020-08-11 08:43:07 +0700

    Thuong

---
+++ views/1.6.php
@@ -0,0 +1,38 @@
+<?php
+include_once './index.php';
+?>
+<div class="ht-container">
+    <div class="ht-ontop">
+        <div class="my-container">
+            <div class="ht-title">
+                <h1 class="mg-r-10">앞으로는인</h1>
+                <p>번역을 저장해야합니다</p>
+            </div>
+            <p>앞으로는 로그인 한 상태에서만</p>
+        </div>
+    </div>
+    <div class="my-container">
+        <div class="list-box-1">
+            <div class="ht-col-2 box-1">
+                <img src="../public/img/ht-images/1.6-01.jpg" alt="">
+                <span>삭제되므로</span>
+            </div>
+            <div class="ht-col-2 box-1">
+                <img src="../public/img/ht-images/1.6-02.jpg" alt="">
+                <span>있도록 염두에두</span>
+            </div>
+            <div class="ht-col-2 box-1">
+                <img src="../public/img/ht-images/1.6-03.jpg" alt="">
+                <span>있으며 내 활동</span>
+            </div>
+            <div class="ht-col-2 box-1">
+                <img src="../public/img/ht-images/1.6-04.jpg" alt="">
+                <span>있염두에두</span>
+            </div>
+            <div class="ht-col-2 box-1">
+                <img src="../public/img/ht-images/1.6-05.jpg" alt="">
+                <span>있으활동</span>
+            </div>
+        </div>
+    </div>
+</div>
