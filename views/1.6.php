edff8e12b0290f6fb0df18c15763b6e9b6ed0445
Hoài Thương <41870695+noname181@users.noreply.github.com>
2020-08-11 14:14:46 +0700

    Thuong

--- views/1.6.php
+++ views/1.6.php
@@ -5,7 +5,7 @@ include_once './index.php';
     <div class="ht-ontop">
         <div class="my-container">
             <div class="ht-title">
-                <h1 class="mg-r-10">앞으123로는인</h1>
+                <h1 class="mg-r-10">앞으로는인</h1>
                 <p>번역을 저장해야합니다</p>
             </div>
             <p>앞으로는 로그인 한 상태에서만</p>
@@ -33,6 +33,72 @@ include_once './index.php';
                 <img src="../public/img/ht-images/1.6-05.jpg" alt="">
                 <span>있으활동</span>
             </div>
+        </div>
+        <div class="list-img">
+            <h2>수 없습니다</h2>
+            <div class="ht-list-img">
+                <div class="ht-col-2 img-1 ht-1">
+                    <span>삭로</span>
+                </div>
+                <div class="ht-col-2 img-2 ht-1">
+                    <span>삭제</span>
+                </div>
+                <div class="ht-col-2 img-1 ht-1">
+                    <span>므로</span>
+                </div>
+                <div class="ht-col-2 img-2 ht-1">
+                    <span>삭로</span>
+                </div>
+                <div class="ht-col-2 img-1 ht-1">
+                    <span>삭로</span>
+                </div>
+                <div class="ht-col-2 img-2 ht-1">
+                    <span>삭로</span>
+                </div>
+                <div class="ht-col-2 img-1 ht-1">
+                    <span>삭로</span>
+                </div>
+                <div class="ht-col-2 img-2 ht-1">
+                    <span>삭로</span>
+                </div>
+                <div class="ht-col-2 img-1 ht-1">
+                    <span>삭로</span>
+                </div>
+                <div class="ht-col-2 img-2 ht-1">
+                    <span>삭로</span>
+                </div>
+
+            </div>
+        </div>
+        <div class="list-img">
+            <h2>수 없습니다</h2>
+            <div class="ht-slider owl-carousel owl-theme">
+                <div class="ht-1">
+                    <div class="img-1 img"></div>
+                </div>
+                <div class="ht-1">
+                    <div class="img-2 img"></div>
+                </div>
+                <div class="ht-1">
+                    <div class="img-1 img"></div>
+                </div>
+                <div class="ht-1">
+                    <div class="img-2 img"></div>
+                </div>
+                <div class="ht-1">
+                    <div class="img-1 img"></div>
+                </div>
+                <div class="ht-1">
+                    <div class="img-2 img"></div>
+                </div>
+                
+            </div>
+        </div>
+        <div class="ht-bot-footer">
+            <a href="#">
+                달과 별을 향해 짜는 손
+            </a>
+
         </div>
     </div>
 </div>
