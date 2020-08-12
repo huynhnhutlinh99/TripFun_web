cf420a8a392e05c275e2551219464a655598108e
noname <l.h.thuong181@gmail.com>
2020-08-12 08:14:26 +0700

    Thuong

--- includes/footer.php
+++ includes/footer.php
@@ -40,24 +40,28 @@
                         <div class="nl-n-green nl-icon"></div>
                         <a href="#" class="nl-item__link">안녕하세요 세계</a>
                     </div>
-                    
+
                 </div>
             </div>
         </div>
     </div>
     <div class="nl-grey-part">
         <div class="my-container">
-            <span class="nl-text-at-bt__footer">케이투어엘엘씨 유한회사</span> <i class="line-single">|</i> 
-            <span class="nl-text-at-bt__footer">사업자등록번호 : *** - **- **** 사업자정보확인</span> <i class="line-single">|</i> 
+            <span class="nl-text-at-bt__footer">케이투어엘엘씨 유한회사</span> <i class="line-single">|</i>
+            <span class="nl-text-at-bt__footer">사업자등록번호 : *** - **- **** 사업자정보확인</span> <i class="line-single">|</i>
             <span class="nl-text-at-bt__footer">대표 :</span>
             <span class="nl-text-at-bt__footer"> 통신판매업신고번호 :</span>
-            <span class="nl-text-at-bt__footer">대표번호 : 000-0000 (한국) 000-000-0000 (미국)</span> <i class="line-single">|</i> 
+            <span class="nl-text-at-bt__footer">대표번호 : 000-0000 (한국) 000-000-0000 (미국)</span> <i
+                class="line-single">|</i>
             <span class="nl-text-at-bt__footer">고객센터 ****@tripfun.net</span>
             <span class="nl-text-at-bt__footer">카카오톡 ID : ******* </span>
-            <span class="nl-text-at-bt__footer">상담 가능시간(한국) : 09:00 ~ 18:00 (주말/공휴일 제외) </span> <i class="line-single">|</i> 
+            <span class="nl-text-at-bt__footer">상담 가능시간(한국) : 09:00 ~ 18:00 (주말/공휴일 제외) </span> <i
+                class="line-single">|</i>
             <span class="nl-text-at-bt__footer">주소</span>
-            <p class="nl-text-at-bt__footer">TripFun 은 통신판매 중개자이며 통신판매의 당사자가 아닙니다. 그러므로 TripFun 은 상품·거래정보 및 거래에 대하여 일체 책임을 지지 않습니다.</p>
+            <p class="nl-text-at-bt__footer">TripFun 은 통신판매 중개자이며 통신판매의 당사자가 아닙니다. 그러므로 TripFun 은 상품·거래정보 및 거래에 대하여 일체
+                책임을 지지 않습니다.</p>
         </div>
     </div>
 </footer>
+
 </html>
--- views/index.php
+++ views/index.php
@@ -7,13 +7,14 @@
     <title>Document</title>
     <link rel="stylesheet" type="text/css" href="../public/css/ht-css/ht-style.css">
     <link rel="stylesheet" type="text/css" href="../public/css/common.css">
-    <script src="https://code.jquery.com/jquery-3.5.1.js" integrity="sha256-QWo7LDvxbWT2tbbQ97B53yJnYU3WhH/C8ycbRAkjPDc=" crossorigin="anonymous"></script>
+    <script src="https://code.jquery.com/jquery-3.5.1.js"
+        integrity="sha256-QWo7LDvxbWT2tbbQ97B53yJnYU3WhH/C8ycbRAkjPDc=" crossorigin="anonymous"></script>
 
     <!-- Carousel slider -->
-    <script type="text/javascript" src="../public/js/OwlCarousel2-2.3.4/owl.carousel.js" ></script>
+    <script type="text/javascript" src="../public/js/OwlCarousel2-2.3.4/owl.carousel.js"></script>
     <link rel="stylesheet" type="text/css" href="../public/js/OwlCarousel2-2.3.4/owl.carousel.css">
     <link rel="stylesheet" type="text/css" href="../public/js/OwlCarousel2-2.3.4/owl.theme.default.css">
-    <script type="text/javascript" src="../public/js/ht-script.js" ></script>
+    <script type="text/javascript" src="../public/js/ht-script.js"></script>
 
 </head>
 
