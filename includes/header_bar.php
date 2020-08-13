c9e33effe4bbdf0321a09bccfb7f60cf24472e4e
huynhnhutlinh99 <40021780+huynhnhutlinh99@users.noreply.github.com>
2020-08-13 14:24:30 +0700

    linh update 1.5

--- includes/header_bar.php
+++ includes/header_bar.php
@@ -1,4 +1,30 @@
-<?php include_once('./head.meta.php')
-<div class="nl-header-bar">
-
+<div class="my-container">
+    <div class="nl-header-bar">
+        <div class="nl-top-line">
+            <div class="nl-left-part">
+                <a href="#" class="nl-logo"></a>
+                <a href="#" class="nl-item__link">여행후기</a>
+                <a href="#" class="nl-item__link">전체 여행지 </a>
+            </div>
+            <div class="nl-right-part"> 
+                <a href="#" class="nl-item__link">파트너 등록</a>
+                <a href="#" class="nl-item__link">로그인</a>
+                <a href="#" class="nl-item__link">회원가입</a>
+            </div>
+        </div>
+        <div class="nl-bottom-line">
+            <div class="nl-left-part">
+                <a href="#" class="nl-item__link">투어&티켓</a>
+                <a href="#" class="nl-item__link">숙소</a>
+                <a href="#" class="nl-item__link">패키지여행</a>
+                <a href="#" class="nl-item__link">여행편의</a>
+                <a href="#" class="nl-item__link">테마여행</a>
+                <a href="#" class="nl-item__link">임박세일</a>
+            </div>
+            <div class="nl-right-part"> 
+                <a href="#" class="nl-item__link">쿠폰/이벤트</a>
+                <a href="#" class="nl-item__link">캐쉬백/리워드</a>
+            </div>
+        </div>
+    </div>
 </div>
