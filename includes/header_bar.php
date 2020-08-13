469533357de7f13731d9af1caeb849d86fe75da8
huynhnhutlinh99 <40021780+huynhnhutlinh99@users.noreply.github.com>
2020-08-13 15:14:23 +0700

    upload header

--- includes/header_bar.php
+++ includes/header_bar.php
@@ -2,14 +2,16 @@
     <div class="nl-header-bar">
         <div class="nl-top-line">
             <div class="nl-left-part">
-                <a href="#" class="nl-logo"></a>
+                <a href="#" class="nl-logo">
+                    <figure class=nl-logo-page></figure>
+                </a>
                 <a href="#" class="nl-item__link">여행후기</a>
                 <a href="#" class="nl-item__link">전체 여행지 </a>
             </div>
             <div class="nl-right-part"> 
                 <a href="#" class="nl-item__link">파트너 등록</a>
                 <a href="#" class="nl-item__link">로그인</a>
-                <a href="#" class="nl-item__link">회원가입</a>
+                <a href="#" class="nl-btn-priamry">회원가입</a>
             </div>
         </div>
         <div class="nl-bottom-line">
@@ -22,7 +24,7 @@
                 <a href="#" class="nl-item__link">임박세일</a>
             </div>
             <div class="nl-right-part"> 
-                <a href="#" class="nl-item__link">쿠폰/이벤트</a>
+                <a href="#" class="nl-item__link active">쿠폰/이벤트</a>
                 <a href="#" class="nl-item__link">캐쉬백/리워드</a>
             </div>
         </div>
