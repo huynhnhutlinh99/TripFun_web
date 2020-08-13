7338d2480795278bdc0748721640f778b11339c0
huynhnhutlinh99 <40021780+huynhnhutlinh99@users.noreply.github.com>
2020-08-13 16:40:01 +0700

    update 1.5 footer header

--- includes/footer.php
+++ includes/footer.php
@@ -23,22 +23,21 @@
                     <a href="#" class="nl-item__link">취소 및 환불정책</a>
                 </div>
                 <div class="nl-ld-col__row5 nl-md-col__row5 nl-sm-col__row5 nl-xs-col__row5">
-                    <p class="nl-title-ft">안녕하세요 세계</p>
                     <div class="my-row">
                         <div class="nl-intag nl-icon"></div>
-                        <a href="#" class="nl-item__link">안녕하세요 세계</a>
+                        <a href="#" class="nl-item__link">인스타그램</a>
                     </div>
                     <div class="my-row">
                         <div class="nl-fb nl-icon"></div>
-                        <a href="#" class="nl-item__link">안녕하세요 세계</a>
+                        <a href="#" class="nl-item__link">페이스북</a>
                     </div>
                     <div class="my-row">
                         <div class="nl-n-black nl-icon"></div>
-                        <a href="#" class="nl-item__link">안녕하세요 세계</a>
+                        <a href="#" class="nl-item__link">네이버 블로그</a>
                     </div>
                     <div class="my-row">
                         <div class="nl-n-green nl-icon"></div>
-                        <a href="#" class="nl-item__link">안녕하세요 세계</a>
+                        <a href="#" class="nl-item__link">네이버 포스트</a>
                     </div>
 
                 </div>
@@ -51,14 +50,13 @@
             <span class="nl-text-at-bt__footer">사업자등록번호 : *** - **- **** 사업자정보확인</span> <i class="line-single">|</i>
             <span class="nl-text-at-bt__footer">대표 :</span>
             <span class="nl-text-at-bt__footer"> 통신판매업신고번호 :</span>
-            <span class="nl-text-at-bt__footer">대표번호 : 000-0000 (한국) 000-000-0000 (미국)</span> <i
-                class="line-single">|</i>
+            <span class="nl-text-at-bt__footer">대표번호 : 000-0000 (한국) 000-000-0000 (미국)</span> <br> <i class="line-single">|</i>
             <span class="nl-text-at-bt__footer">고객센터 ****@tripfun.net</span>
             <span class="nl-text-at-bt__footer">카카오톡 ID : ******* </span>
             <span class="nl-text-at-bt__footer">상담 가능시간(한국) : 09:00 ~ 18:00 (주말/공휴일 제외) </span> <i
                 class="line-single">|</i>
             <span class="nl-text-at-bt__footer">주소</span>
-            <p class="nl-text-at-bt__footer">TripFun 은 통신판매 중개자이며 통신판매의 당사자가 아닙니다. 그러므로 TripFun 은 상품·거래정보 및 거래에 대하여 일체
+            <p class="nl-text-at-bt__footer nl-line-end">TripFun 은 통신판매 중개자이며 통신판매의 당사자가 아닙니다. 그러므로 TripFun 은 상품·거래정보 및 거래에 대하여 일체
                 책임을 지지 않습니다.</p>
         </div>
     </div>
