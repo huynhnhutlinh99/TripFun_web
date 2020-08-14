0d177649558bfb2b085fc6578f35cf849ddf1310
huynhnhutlinh99 <40021780+huynhnhutlinh99@users.noreply.github.com>
2020-08-14 16:29:30 +0700

    update 1.10

--- views/1.10.php
+++ views/1.10.php
@@ -2,6 +2,7 @@
 <div class="type-1-10">
     <div class="my-container">
         <h3 class="nl-title">전체 여행지 내용?</h3>
+        <button class="nl-btn-x-pu"></button>
         <div class="nl-multi-tab">
             <div class="nl-head-tab">
                 <div class="nl-head-item">
@@ -42,6 +43,68 @@
                         <a href="#" class="nl-city">솔트레이크시티</a>
                         <a href="#" class="nl-city">그랜드캐년 경비행기/헬기/버스투어</a>
                         <a href="#" class="nl-city">라스베가스 쇼티켓 </a>
+                    </div>
+                </div>
+                <div class="nl-ctn-item">
+                    <div class="nl-col-1"><a href="#" class="nl-country">국립공원</a></div>
+                    <div class="nl-col-2">
+                        <a href="#" class="nl-city">그랜드캐년</a>
+                        <a href="#" class="nl-city">요세미티</a>
+                        <a href="#" class="nl-city">옐로우스톤</a>
+                        <a href="#" class="nl-city">자이언캐년</a>
+                        <a href="#" class="nl-city">브라이스캐년</a>
+                        <a href="#" class="nl-city">데스밸리</a>
+                        <a href="#" class="nl-city">모뉴멘트밸리</a>
+                        <a href="#" class="nl-city">아치스캐년</a>
+                        <a href="#" class="nl-city">캐년랜드</a>
+                        <a href="#" class="nl-city">에버글래이즈</a>
+                        <a href="#" class="nl-city">록키</a>
+                    </div>
+                </div>
+                <div class="nl-ctn-item">
+                    <div class="nl-col-1"><a href="#" class="nl-country">캐나다</a></div>
+                    <div class="nl-col-2">
+                        <a href="#" class="nl-city">밴쿠버</a>
+                        <a href="#" class="nl-city">토론토</a>
+                    </div>
+                </div>
+                <div class="nl-ctn-item">
+                    <div class="nl-col-1"><a href="#" class="nl-country">멕시코</a></div>
+                    <div class="nl-col-2">
+                        <a href="#" class="nl-city">칸쿤</a>
+                        <a href="#" class="nl-city">멕시코시티</a>
+                        <a href="#" class="nl-city">플라야 델 카르멘</a>
+                    </div>
+                </div>
+                <div class="nl-ctn-item">
+                    <div class="nl-col-1"><a href="#" class="nl-country">브라질</a></div>
+                    <div class="nl-col-2">
+                        <a href="#" class="nl-city">리오데자네이루</a>
+                        <a href="#" class="nl-city">상파울루</a>
+                    </div>
+                </div>
+                <div class="nl-ctn-item">
+                    <div class="nl-col-1"><a href="#" class="nl-country">아르헨티나</a></div>
+                    <div class="nl-col-2">
+                        <a href="#" class="nl-city">부에노스아이레스</a>
+                    </div>
+                </div>
+                <div class="nl-ctn-item">
+                    <div class="nl-col-1"><a href="#" class="nl-country">칠레</a></div>
+                    <div class="nl-col-2">
+                        <a href="#" class="nl-city">산티아고</a>
+                    </div>
+                </div>
+                <div class="nl-ctn-item">
+                    <div class="nl-col-1"><a href="#" class="nl-country">코스타리카</a></div>
+                    <div class="nl-col-2">
+                        <a href="#" class="nl-city">산호세</a>
+                    </div>
+                </div>
+                <div class="nl-ctn-item">
+                    <div class="nl-col-1"><a href="#" class="nl-country">쿠바</a></div>
+                    <div class="nl-col-2">
+                        <a href="#" class="nl-city">아바나</a>
                     </div>
                 </div>
             </div>
