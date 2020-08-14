09c10b509b7e236b7f0880869a3a7b6ed2803da0
Hoài Thương <41870695+noname181@users.noreply.github.com>
2020-08-14 09:27:17 +0700

    1.2 page

--- views/1.4.php
+++ views/1.4.php
@@ -1,87 +1,52 @@
 <?php
 include_once './index.php';
 ?>
-<div class="ht-container">
-    <div class="ht-ontop-img type-1">
-        <div class="ht-banner" style="background-image:url(../public/img/ht-images/1.1.jpg);"></div>
-        <div class="my-container">
-            <div class="ht-content">
-                <div class="part-1">
-                    <a href="#">업그레이드 중</a>
-                    <div class="ht-right">
-                        <div class="ht-link-2">록을 중앙에서 관리 할 수​​있습니 <img src="../public/img/ht-images/1.1-arrow.png"
-                                alt=""></div>
-                        <div class="ht-link-2">나중에 쉽게 액세<img src="../public/img/ht-images/1.1-arrow.png" alt=""></div>
-                    </div>
-
+<div class="ht-container ht-border">
+    <div class="my-container ">
+        <div class="ht-list-box-1 type-2">
+            <div class="ht-col-2 box-1 active">
+                <img src="../public/img/ht-images/1.6-01.jpg" alt="">
+                <span>삭제되므로</span>
+            </div>
+            <div class="ht-col-2 box-1">
+                <img src="../public/img/ht-images/1.6-02.jpg" alt="">
+                <span>있도록 염두에두</span>
+            </div>
+            <div class="ht-col-2 box-1">
+                <img src="../public/img/ht-images/1.6-03.jpg" alt="">
+                <span>있으며 내 활동</span>
+            </div>
+            <div class="ht-col-2 box-1">
+                <img src="../public/img/ht-images/1.6-04.jpg" alt="">
+                <span>있염두에두</span>
+            </div>
+            <div class="ht-col-2 box-1">
+                <img src="../public/img/ht-images/1.6-05.jpg" alt="">
+                <span>있으활동</span>
+            </div>
+            <div class="ht-col-2 box-1">
+                <img src="../public/img/ht-images/1.6-05.jpg" alt="">
+                <span>있으활동</span>
+            </div>
+        </div>
+        <div class="ht-contain-banner p1_4">
+            <!-- <div class="ht-content">
+                <div class="ht-title">
+                    수줍음도 잊어 다가가욕망과
                 </div>
-                <div class="part-2">
-                    <div class="ht-col-ct">
-                        <input type="text" placeholder="중앙에관리할수​​" onfocus="this.placeholder = ''"
-                            onblur="this.placeholder = '록을중'">
-                    </div>
-                    <div class="ht-col-ct">
-                        <div class="date">
-                            <img src="../public/img/ht-images/1.2-calen.png" alt="">
-                            <input type="text" placeholder="록을중" onfocus="this.placeholder = ''"
-                                onblur="this.placeholder = '록을중'">
-                            <img src="../public/img/ht-images/1.2-gray-arrow.png" alt="">
-                            <input type="text" placeholder="록을중" onfocus="this.placeholder = ''"
-                                onblur="this.placeholder = '록을중'">
+                <span class="sub-title">레는욕망과 욕망과득 차</span>
+                <a href="#">욕망과욕</a>
+            </div> -->
 
-                        </div>
-                        <button class="ht-submit">관리</button>
-                    </div>
+            <div class="owl-carousel owl-theme ht-banners">
+                <div class="ht-banner" style="background-image: url(../public/img/ht-images/banner-1.jpg);">
                 </div>
-                <div class="part-3">
-                    <ul class="ht-menu">
-                        <li class="active">삭제되</li>
-                        <li>쉽게 액세스</li>
-                        <li>태에</li>
-                        <li>태에</li>
-                        <li>쉽게 액세스</li>
-                        <li>쉽게 액세스</li>
-                        <li>삭제</li>
-                    </ul>
+                <div class="ht-banner" style="background-image: url(../public/img/ht-images/banner-1.jpg);">
                 </div>
             </div>
         </div>
-    </div>
-    <div class="my-container">
 
-
-
-
-        <div class="ht-banner-2">
-            <div class="ht-left"
-                style="background-image: url(https://images3.alphacoders.com/776/thumb-1920-776272.jpg);">
-                <div class="ht-content">
-                    <div class="ht-title">
-                        액세스 할 수 있도록 염두에두고 <br>싶은 번역을 저장해야합니다
-                    </div>
-                    <div class="sub-title">
-                        태에서만 번역 기록을 볼 수
-                    </div>
-                </div>
-
-            </div>
-            <div class="ht-right">
-                <div class="ht-top"
-                    style="background-image: url(https://images.unsplash.com/photo-1521208916306-71fce562015a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&w=1000&q=80);">
-                    <div class="ht-content">
-                        있으며 내 활동 섹션에 <br>서 번역 <strong>20,000</strong> 중앙<br>에서 관리 할 수 ​​있습니다
-                    </div>
-                </div>
-                <div class="ht-bot"
-                    style="background-image: url(https://images.unsplash.com/photo-1521208916306-71fce562015a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&w=1000&q=80);">
-                    <div class="ht-content">
-                        있으며 내 활동 섹션에 <br>서 번역 기록을 중앙<br>에서 관리 할 수 ​​있습니다
-                    </div>
-                </div>
-            </div>
-        </div>
-        <div class="ht-list type-2">
-            <h2>수 없습니다</h2>
+        <div class="ht-list">
             <ul class="ht-menu">
                 <li class="active">삭제되</li>
                 <li>쉽게 액세스</li>
@@ -89,14 +54,15 @@ include_once './index.php';
                 <li>태에</li>
                 <li>쉽게 액세스</li>
                 <li>쉽게 액세스</li>
+                <li>삭제</li>
             </ul>
-            <a href="#" class="ht-direct">다가가우리 <span class="ht-icon-r"></span></a>
+            <h2>수 없습니다</h2>
             <div class="ht-slider owl-carousel type-1 owl-theme">
                 <div class="ht-item">
                     <div class="img-2 ht-img">
                         <div class="ht-time">천국을보: 2020-05-03 ~ 2020-05-24</div>
                     </div>
-                    <span class="vote unlike"></span>
+                    <span class="like"></span>
                     <div class="content">
                         <div class="ht-gray">
                             <div class="ht-star">
@@ -107,8 +73,7 @@ include_once './index.php';
                         </div>
                         <div class="ht-title">영혼을 달과 욕망으로별점점 듭니다을 (향해짜는손)</div>
                         <div class="ht-option">영혼을 <img src="../public/img/ht-images/1.1-set.svg" alt=""></div>
-                        <div class="ht-price"><span class="discount">270,000 을</span>370,000<span class="sub">/1
-                                을</span>
+                        <div class="ht-price"><span class="discount">270,000 을</span>370,000<span class="sub">/1 을</span>
                         </div>
                     </div>
                     <span class="promo">천국을보게</span>
@@ -130,54 +95,7 @@ include_once './index.php';
                         </div>
                         <div class="ht-title">영혼을 달과 욕망으로별점점 듭니다을 (향해짜는손)</div>
                         <div class="ht-option">영혼을 <img src="../public/img/ht-images/1.1-set.svg" alt=""></div>
-                        <div class="ht-price"><span class="discount">270,000 을</span>370,000<span class="sub">/1
-                                을</span>
-                        </div>
-                    </div>
-                    <span class="promo">천국을보게</span>
-                    <div class="yellow-tick"></div>
-
-                </div>
-                <div class="ht-item">
-                    <div class="img-2 ht-img">
-                        <div class="ht-time">천국을보: 2020-05-03 ~ 2020-05-24</div>
-                    </div>
-                    <span class="vote like"></span>
-                    <div class="content">
-                        <div class="ht-gray">
-                            <div class="ht-star">
-                                <img src="../public/img/ht-images/1.1-star.svg" alt="">
-                                <span class="number">4.8<span class="mg-l-3">(5만)</span></span>
-                            </div>
-                            서서히섞이 . 것같아
-                        </div>
-                        <div class="ht-title">영혼을 달과 욕망으로별점점 듭니다을 (향해짜는손)</div>
-                        <div class="ht-option">영혼을 <img src="../public/img/ht-images/1.1-set.svg" alt=""></div>
-                        <div class="ht-price"><span class="discount">270,000 을</span>370,000<span class="sub">/1
-                                을</span>
-                        </div>
-                    </div>
-                    <span class="promo">천국을보게</span>
-                    <div class="yellow-tick"></div>
-
-                </div>
-                <div class="ht-item">
-                    <div class="img-2 ht-img">
-                        <div class="ht-time">천국을보: 2020-05-03 ~ 2020-05-24</div>
-                    </div>
-                    <span class="vote like"></span>
-                    <div class="content">
-                        <div class="ht-gray">
-                            <div class="ht-star">
-                                <img src="../public/img/ht-images/1.1-star.svg" alt="">
-                                <span class="number">4.8<span class="mg-l-3">(5만)</span></span>
-                            </div>
-                            서서히섞이 . 것같아
-                        </div>
-                        <div class="ht-title">영혼을 달과 욕망으로별점점 듭니다을 (향해짜는손)</div>
-                        <div class="ht-option">영혼을 <img src="../public/img/ht-images/1.1-set.svg" alt=""></div>
-                        <div class="ht-price"><span class="discount">270,000 을</span>370,000<span class="sub">/1
-                                을</span>
+                        <div class="ht-price"><span class="discount">270,000</span>370,000<span class="sub">/1 을</span>
                         </div>
                     </div>
                     <span class="promo">천국을보게</span>
@@ -199,8 +117,51 @@ include_once './index.php';
                         </div>
                         <div class="ht-title">영혼을 달과 욕망으로별점점 듭니다을 (향해짜는손)</div>
                         <div class="ht-option">영혼을 <img src="../public/img/ht-images/1.1-set.svg" alt=""></div>
-                        <div class="ht-price"><span class="discount">270,000 을</span>370,000<span class="sub">/1
-                                을</span>
+                        <div class="ht-price"><span class="discount">270,000 </span>370,000<span class="sub">/1 을</span>
+                        </div>
+                    </div>
+                    <span class="promo">천국을보게</span>
+                    <div class="yellow-tick"></div>
+
+                </div>
+                <div class="ht-item">
+                    <div class="img-2 ht-img">
+                        <div class="ht-time">천국을보: 2020-05-03 ~ 2020-05-24</div>
+                    </div>
+                    <span class="vote like"></span>
+                    <div class="content">
+                        <div class="ht-gray">
+                            <div class="ht-star">
+                                <img src="../public/img/ht-images/1.1-star.svg" alt="">
+                                <span class="number">4.8<span class="mg-l-3">(5만)</span></span>
+                            </div>
+                            서서히섞이 . 것같아
+                        </div>
+                        <div class="ht-title">영혼을 달과 욕망으로별점점 듭니다을 (향해짜는손)</div>
+                        <div class="ht-option">영혼을 <img src="../public/img/ht-images/1.1-set.svg" alt=""></div>
+                        <div class="ht-price"><span class="discount">270,000 </span>370,000<span class="sub">/1 을</span>
+                        </div>
+                    </div>
+                    <span class="promo">천국을보게</span>
+                    <div class="yellow-tick"></div>
+
+                </div>
+                <div class="ht-item">
+                    <div class="img-2 ht-img">
+                        <div class="ht-time">천국을보: 2020-05-03 ~ 2020-05-24</div>
+                    </div>
+                    <span class="vote like"></span>
+                    <div class="content">
+                        <div class="ht-gray">
+                            <div class="ht-star">
+                                <img src="../public/img/ht-images/1.1-star.svg" alt="">
+                                <span class="number">4.8<span class="mg-l-3">(5만)</span></span>
+                            </div>
+                            서서히섞이 . 것같아
+                        </div>
+                        <div class="ht-title">영혼을 달과 욕망으로별점점 듭니다을 (향해짜는손)</div>
+                        <div class="ht-option">영혼을 <img src="../public/img/ht-images/1.1-set.svg" alt=""></div>
+                        <div class="ht-price"><span class="discount">270,000 </span>370,000<span class="sub">/1 을</span>
                         </div>
                     </div>
                     <span class="promo">천국을보게</span>
@@ -222,8 +183,7 @@ include_once './index.php';
                         </div>
                         <div class="ht-title">영혼을 달과 욕망으로별점점 듭니다을 (향해짜는손)</div>
                         <div class="ht-option">영혼을 <img src="../public/img/ht-images/1.1-set.svg" alt=""></div>
-                        <div class="ht-price"><span class="discount">270,000 을</span>370,000<span class="sub">/1
-                                을</span>
+                        <div class="ht-price"><span class="discount">270,000 </span>370,000<span class="sub">/1 을</span>
                         </div>
                     </div>
                     <span class="promo">천국을보게</span>
@@ -232,22 +192,12 @@ include_once './index.php';
                 </div>
             </div>
         </div>
+        <div class="ht-bot-footer">
+            <a href="#">
+                달과 별을 향해 짜는 손
+            </a>
 
-
-        <div class="ht-list ht-1_2">
-            <h2>수 없습니다</h2>
-            <div class="ht-list-img type-2">
-                <div class="ht-col-2 img-1 ht-img">
-                    <span>삭로</span>
-
-                </div>
-                <div class="ht-col-2 img-2 ht-img">
-                    <span>삭로</span>
-
-                </div>
-            </div>
         </div>
-
     </div>
 </div>
 
