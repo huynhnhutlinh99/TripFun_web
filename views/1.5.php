b349b6bd23cbb0aecf4e245a6a04a1349bbf782d
huynhnhutlinh99 <40021780+huynhnhutlinh99@users.noreply.github.com>
2020-08-14 17:22:44 +0700

    update 1.5

--- views/1.5.php
+++ views/1.5.php
@@ -44,8 +44,8 @@
                         <div class="nl-img-part">
                             <figure class="nl-pic-item"></figure>
                         </div>
-                        <h3 class="nl-name">그랜드캐년 여행</h3>
-                        <div class="nl-des">버킷리스트~ 그랜드캐년 인기여행</div>
+                        <h3 class="nl-name">라스베가스 할인 쇼</h3>
+                        <div class="nl-des">라스베가스 할인 쇼티켓이 한자리에</div>
                     </div>
                 </div>
                 <div class="my-col-lg-4 my-col-md-4 my-col-sm-12 my-col-xs-12">
@@ -53,8 +53,8 @@
                         <div class="nl-img-part">
                             <figure class="nl-pic-item"></figure>
                         </div>
-                        <h3 class="nl-name">그랜드캐년 여행</h3>
-                        <div class="nl-des">버킷리스트~ 그랜드캐년 인기여행</div>
+                        <h3 class="nl-name">LA 테마파크 할인티켓</h3>
+                        <div class="nl-des">테마파크 티켓 / 투어를 착한가격에</div>
                     </div>
                 </div>
                 <div class="my-col-lg-4 my-col-md-4 my-col-sm-12 my-col-xs-12">
@@ -62,8 +62,71 @@
                         <div class="nl-img-part">
                             <figure class="nl-pic-item"></figure>
                         </div>
-                        <h3 class="nl-name">그랜드캐년 여행</h3>
-                        <div class="nl-des">버킷리스트~ 그랜드캐년 인기여행</div>
+                        <h3 class="nl-name">낭만 LA 여행</h3>
+                        <div class="nl-des">오늘 라라랜드 주인공이 되어 보자</div>
+                    </div>
+                </div>
+                <div class="my-col-lg-4 my-col-md-4 my-col-sm-12 my-col-xs-12">
+                    <div class="nl-item">
+                        <div class="nl-img-part">
+                            <figure class="nl-pic-item"></figure>
+                        </div>
+                        <h3 class="nl-name">힐링 하와이</h3>
+                        <div class="nl-des">하와이 인기 투어 / 스냅여행</div>
+                    </div>
+                </div>
+                <div class="my-col-lg-4 my-col-md-4 my-col-sm-12 my-col-xs-12">
+                    <div class="nl-item">
+                        <div class="nl-img-part">
+                            <figure class="nl-pic-item"></figure>
+                        </div>
+                        <h3 class="nl-name">그랜드캐년 경비행기/헬기/버스투어 </h3>
+                        <div class="nl-des">트립펀으로 예약! 다국적 투어 두렵지 않아요</div>
+                    </div>
+                </div>
+                <div class="my-col-lg-4 my-col-md-4 my-col-sm-12 my-col-xs-12">
+                    <div class="nl-item">
+                        <div class="nl-img-part">
+                            <figure class="nl-pic-item"></figure>
+                        </div>
+                        <h3 class="nl-name">찐한 뉴욕여행</h3>
+                        <div class="nl-des">빅 애플 뉴욕여행 다모였네</div>
+                    </div>
+                </div>
+                <div class="my-col-lg-4 my-col-md-4 my-col-sm-12 my-col-xs-12">
+                    <div class="nl-item">
+                        <div class="nl-img-part">
+                            <figure class="nl-pic-item"></figure>
+                        </div>
+                        <h3 class="nl-name">감동 뉴욕 뮤지컬</h3>
+                        <div class="nl-des">브로드웨이 감동을 담아 담아서</div>
+                    </div>
+                </div>
+                <div class="my-col-lg-4 my-col-md-4 my-col-sm-12 my-col-xs-12">
+                    <div class="nl-item">
+                        <div class="nl-img-part">
+                            <figure class="nl-pic-item"></figure>
+                        </div>
+                        <h3 class="nl-name">웅장한 요세미티</h3>
+                        <div class="nl-des">요세미티에 쏟아지는 밤하늘의 별빛</div>
+                    </div>
+                </div>
+                <div class="my-col-lg-4 my-col-md-4 my-col-sm-12 my-col-xs-12">
+                    <div class="nl-item">
+                        <div class="nl-img-part">
+                            <figure class="nl-pic-item"></figure>
+                        </div>
+                        <h3 class="nl-name">인기 샌프란시스코 여행</h3>
+                        <div class="nl-des">샌프란시스코 / 근교 투어 총 집합</div>
+                    </div>
+                </div>
+                <div class="my-col-lg-4 my-col-md-4 my-col-sm-12 my-col-xs-12">
+                    <div class="nl-item">
+                        <div class="nl-img-part">
+                            <figure class="nl-pic-item"></figure>
+                        </div>
+                        <h3 class="nl-name">미국 공항픽업 / 샌딩</h3>
+                        <div class="nl-des">숙소에서 공항까지 안전하고 편안하게</div>
                     </div>
                 </div>
             </div>
